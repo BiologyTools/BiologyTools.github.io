c81145c73859f6f293f8fb26e72ecd134a306299
RepoErik <84872500+BiologyTools@users.noreply.github.com>
2024-02-02 07:40:46 +0200

    BioGTK 4.3.0 Documentation

---
+++ Documentation/BioGTK-4.3.0/html/search/functions_b.js
@@ -0,0 +1,23 @@
+var searchData=
+[
+  ['onlocaldeleteevent_0',['OnLocalDeleteEvent',['../class_bio_g_t_k_1_1_node_view.html#a07eaa3d0dec13e2bd192efbdf1a284a8',1,'BioGTK.NodeView.OnLocalDeleteEvent()'],['../class_bio_g_t_k_1_1_recorder.html#ab401bf3f0f35e204716934fc497eff52',1,'BioGTK.Recorder.OnLocalDeleteEvent()']]],
+  ['open_1',['open',['../class_bio_g_t_k_1_1_image_j_1_1_roi_decoder.html#a7a9011c57589b1d6f336e48c730b278c',1,'BioGTK::ImageJ::RoiDecoder']]],
+  ['open_2',['Open',['../class_bio_g_t_k_1_1_bio_image.html#a65af7d7f94c4d578bd33f834fead5eb1',1,'BioGTK.BioImage.Open(string file)'],['../class_bio_g_t_k_1_1_bio_image.html#aa2b157f471b54f6c6e1488594177d853',1,'BioGTK.BioImage.Open(string[] files)'],['../class_bio_g_t_k_1_1_tabs_view.html#a30006cee6339b88f8c91fb91109b5f5c',1,'BioGTK.TabsView.Open()']]],
+  ['openasync_3',['OpenAsync',['../class_bio_g_t_k_1_1_bio_image.html#a825bb12bd01fe6a3f185dfffc850602c',1,'BioGTK.BioImage.OpenAsync(string file, bool OME, bool newtab, bool images)'],['../class_bio_g_t_k_1_1_bio_image.html#ac975818e9f78f2d328b8943d0ede89b8',1,'BioGTK.BioImage.OpenAsync(string[] files, bool OME, bool tab, bool images)']]],
+  ['openfile_4',['OpenFile',['../class_bio_g_t_k_1_1_bio_image.html#a00787c66ba2458500aa07ff4a9215f41',1,'BioGTK.BioImage.OpenFile(string file)'],['../class_bio_g_t_k_1_1_bio_image.html#a3c2262a7be99c6bc4944a956b006d5e7',1,'BioGTK.BioImage.OpenFile(string file, int series, bool tab, bool addToImages)'],['../class_bio_g_t_k_1_1_bio_image.html#a493dc5424950901c82b03d00574beb3a',1,'BioGTK.BioImage.OpenFile(string file, int series, bool tab, bool addToImages, bool tile, int tileX, int tileY, int tileSizeX, int tileSizeY)']]],
+  ['openfrombytearray_5',['openFromByteArray',['../class_bio_g_t_k_1_1_image_j_1_1_roi_decoder.html#a23c2da1634ca0ed9938ce763f1ee7fab',1,'BioGTK::ImageJ::RoiDecoder']]],
+  ['openimagesmenuclick_6',['openImagesMenuClick',['../class_bio_g_t_k_1_1_tabs_view.html#a7f74351dca44ada13a82908ff408a53b',1,'BioGTK::TabsView']]],
+  ['openome_7',['OpenOME',['../class_bio_g_t_k_1_1_bio_image.html#ae5f03de00297bdaa7bb55532df259cbd',1,'BioGTK.BioImage.OpenOME(string file, bool tab)'],['../class_bio_g_t_k_1_1_bio_image.html#a050db5c3ef02a9ab99fab5e4d94585ec',1,'BioGTK.BioImage.OpenOME(string file, int serie)'],['../class_bio_g_t_k_1_1_bio_image.html#aaad71e1b2f501c82226280d41d13a2f4',1,'BioGTK.BioImage.OpenOME(string file, int serie, bool tab, bool addToImages, bool tile, int tilex, int tiley, int tileSizeX, int tileSizeY)']]],
+  ['openomeimagesmenuclick_8',['openOMEImagesMenuClick',['../class_bio_g_t_k_1_1_tabs_view.html#a9734ffc8d9368c7215541cd89ac5d98c',1,'BioGTK::TabsView']]],
+  ['openomerois_9',['OpenOMEROIs',['../class_bio_g_t_k_1_1_bio_image.html#a4ae407cdf7232be8f2f860a36d7f5696',1,'BioGTK::BioImage']]],
+  ['openomeseries_10',['OpenOMESeries',['../class_bio_g_t_k_1_1_bio_image.html#ad41110891b5d51633fdb4cbf5117d277',1,'BioGTK::BioImage']]],
+  ['openomeseriesmenuclick_11',['openOMESeriesMenuClick',['../class_bio_g_t_k_1_1_tabs_view.html#a5b116b7ffcc0e4edd9dbeac42a39a0c2',1,'BioGTK::TabsView']]],
+  ['openseries_12',['OpenSeries',['../class_bio_g_t_k_1_1_bio_image.html#a8cfa96c61e3f64cbc0791ab5d7da4460',1,'BioGTK::BioImage']]],
+  ['openseriesmenuclick_13',['openSeriesMenuClick',['../class_bio_g_t_k_1_1_tabs_view.html#a60ea4a33e938ad8ba9f9011c622a9940',1,'BioGTK::TabsView']]],
+  ['openvips_14',['OpenVips',['../class_bio_g_t_k_1_1_bio_image.html#a29eed79f83a4dcc52f8b020de90606cc',1,'BioGTK::BioImage']]],
+  ['openxml_15',['OpenXML',['../class_bio_g_t_k_1_1_bio_image.html#a78715e950db2b29afe8d059933040579',1,'BioGTK::BioImage']]],
+  ['operator_2a_16',['operator*',['../class_bio_g_t_k_1_1_bio_image.html#a4c4e4cbfc1d5bee149aed0548c7f6318',1,'BioGTK::BioImage']]],
+  ['operator_2b_17',['operator+',['../class_bio_g_t_k_1_1_bio_image.html#ae9348a3d1957edd347a15ab157bdb63c',1,'BioGTK.BioImage.operator+(BioImage a, float b)'],['../class_bio_g_t_k_1_1_bio_image.html#ad5ff5cba9217359ffc7aca1d20a867b5',1,'BioGTK.BioImage.operator+(BioImage a, ColorS b)']]],
+  ['operator_2d_18',['operator-',['../class_bio_g_t_k_1_1_bio_image.html#a4d1acea696b4928d8a6fe52c5cef15be',1,'BioGTK.BioImage.operator-(BioImage a, float b)'],['../class_bio_g_t_k_1_1_bio_image.html#a05c4f694b6c547b56bdeb41db2766745',1,'BioGTK.BioImage.operator-(BioImage a, ColorS b)']]],
+  ['operator_2f_19',['operator/',['../class_bio_g_t_k_1_1_bio_image.html#a198cddd3b04e5530dc25593c68000f0b',1,'BioGTK.BioImage.operator/(BioImage a, float b)'],['../class_bio_g_t_k_1_1_bio_image.html#a345440bdd76be2050f588b227873c4f1',1,'BioGTK.BioImage.operator/(BioImage a, ColorS b)']]]
+];
