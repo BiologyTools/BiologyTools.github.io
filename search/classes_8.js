40e1aa53e703eec6de8f2340a39c4cc9e8ff5adf
Erik Repo <84872500+BiologyTools@users.noreply.github.com>
2024-10-01 23:31:25 +0300

    BioGTK 5.4.1 Documentation.

--- search/classes_8.js
+++ search/classes_8.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['magicselect_0',['MagicSelect',['../class_bio_1_1_magic_select.html',1,'Bio']]],
-  ['mask_1',['Mask',['../class_bio_g_t_k_1_1_r_o_i_1_1_mask.html',1,'BioGTK::ROI']]],
-  ['model_2',['Model',['../class_bio_g_t_k_1_1_m_l_1_1_m_l_1_1_model.html',1,'BioGTK::ML::ML']]]
+  ['model_1',['Model',['../class_bio_g_t_k_1_1_m_l_1_1_m_l_1_1_model.html',1,'BioGTK::ML::ML']]]
 ];
--- search/functions_3.js
+++ search/functions_3.js
@@ -1,10 +1,5 @@
 var searchData=
 [
-  ['dequeue_0',['Dequeue',['../class_bio_1_1_graphics_1_1_flood_fill_range_queue.html#a6c8a47fb9d20bc9ddd52fb2a63e1c09d',1,'Bio::Graphics::FloodFillRangeQueue']]],
-  ['detectvendor_1',['DetectVendor',['../class_bio_1_1_slide_image.html#acfac70b27119fd341364b5d7da367eff',1,'Bio::SlideImage']]],
-  ['dispose_2',['Dispose',['../struct_bio_1_1_graphics_1_1_pen.html#a0a4360433f3c2698944cdcea6ad2e344',1,'Bio.Graphics.Pen.Dispose()'],['../class_bio_1_1_graphics_1_1_graphics.html#ac48d89029de6e6dbf132b0c8813d1962',1,'Bio.Graphics.Graphics.Dispose()'],['../class_bio_g_t_k_1_1_bio_image.html#adc7c872efd42942ce2ef02830e791c35',1,'BioGTK.BioImage.Dispose()'],['../class_bio_1_1_slide_image.html#ae604dd1f317187b590c6d000fe5ac534',1,'Bio.SlideImage.Dispose(bool disposing)'],['../class_bio_1_1_slide_image.html#a024c435a1e6dac97bd3a57c6230ac522',1,'Bio.SlideImage.Dispose()']]],
-  ['drawellipse_3',['DrawEllipse',['../class_bio_1_1_graphics_1_1_graphics.html#aef1dbe883959fdc5ba849af40d372252',1,'Bio.Graphics.Graphics.DrawEllipse(Rectangle r)'],['../class_bio_1_1_graphics_1_1_graphics.html#abac78d563afee1a651e4043feaab1a5a',1,'Bio.Graphics.Graphics.DrawEllipse(RectangleF r)']]],
-  ['drawline_4',['DrawLine',['../class_bio_1_1_graphics_1_1_graphics.html#a127cfc1f7a4a73e903cd4961dfc72a77',1,'Bio.Graphics.Graphics.DrawLine(int x, int y, int x2, int y2)'],['../class_bio_1_1_graphics_1_1_graphics.html#acd9c6ffb380f1a42c992aad53ef86244',1,'Bio.Graphics.Graphics.DrawLine(PointF p, PointF p2)']]],
-  ['drawrectangle_5',['DrawRectangle',['../class_bio_1_1_graphics_1_1_graphics.html#af5dbc36bd8e31c3c82a42ac6f3991fd0',1,'Bio.Graphics.Graphics.DrawRectangle(Rectangle r)'],['../class_bio_1_1_graphics_1_1_graphics.html#a48a4117e2487a2de9b9c9594108cdc87',1,'Bio.Graphics.Graphics.DrawRectangle(RectangleF r)']]],
-  ['drawscanline_6',['DrawScanline',['../class_bio_1_1_graphics_1_1_graphics.html#ad8b2e64da3bdbc89e111638584e8e676',1,'Bio::Graphics::Graphics']]]
+  ['exportroisoffolderofimagesmenuclick_0',['exportROIsOfFolderOfImagesMenuClick',['../class_bio_g_t_k_1_1_tabs_view.html#a2e5095b7687b28f107c0faa4f94b1171',1,'BioGTK::TabsView']]],
+  ['exportroistocsvmenuclick_1',['exportROIsToCSVMenuClick',['../class_bio_g_t_k_1_1_tabs_view.html#a3655d5f2222df2f1e8df3e0e44a52a75',1,'BioGTK::TabsView']]]
 ];
